f0bcf8554fc65da7a72406bcee39df16faf08abb
Anshul <anshuljain435@gmail.com>
2024-02-16 19:37:10 +0530

    my first commmit

---
+++ Day2_addTwoNo.js
@@ -0,0 +1,6 @@
+const a = 50 ;
+const b = 10;
+
+// add two numbers
+const sum = a+b;
+console.log("The sum of " + a + ' and ' + b + ' is: ' +sum );
---
+++ Day3_toFIndSqrtNo.js
@@ -0,0 +1,9 @@
+function SquareRoot(no) {
+    result = Math.sqrt(no);
+    console.log(result);
+}
+
+SquareRoot(9);
+SquareRoot(12);
+SquareRoot(100);
+SquareRoot(16);
