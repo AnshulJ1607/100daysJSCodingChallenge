50c55fbc39fc9a436ff5a2a3dfe510b69f405ad5
Anshul Jain <42061185+AnshulJ1607@users.noreply.github.com>
2024-02-16 19:41:55 +0530

    Delete 100daysJSCodingChallenge directory

--- 100daysJSCodingChallenge/Day2_addTwoNo.js
+++
@@ -1,6 +0,0 @@
-const a = 50 ;
-const b = 10;
-
-// add two numbers
-const sum = a+b;
-console.log("The sum of " + a + ' and ' + b + ' is: ' +sum );
--- 100daysJSCodingChallenge/Day3_toFIndSqrtNo.js
+++
@@ -1,9 +0,0 @@
-function SquareRoot(no) {
-    result = Math.sqrt(no);
-    console.log(result);
-}
-
-SquareRoot(9);
-SquareRoot(12);
-SquareRoot(100);
-SquareRoot(16);
